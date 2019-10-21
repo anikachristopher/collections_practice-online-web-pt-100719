344e6fd948f37a64326a54ce8d5d785c79e067f9
Anika Christopher <anika.mchristopher@gmail.com>
2019-10-21 02:25:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,12 @@
 def sort_array_asc(integer)
-  integer.sort do |a b|
+  integer.sort do |a, b|
+    if a == b 
+      0 
+    elsif a < b 
+      -1
+    elsif a > b 
+      1 
+  end
 end
 
 def sort_array_desc(integer)
